c68a9ef16f2b734fab88186aa36e4435d54748ac
mauroPh <mauro.philipe.santos@gmail.com>
2022-06-20 21:21:33 -0300

    update repository

--- main.py
+++ main.py
@@ -1,55 +1,50 @@
-from Tools.scripts.dutree import display
 from fastapi import FastAPI
-import pandas as pd
+from pydantic import BaseModel
 
+class Pedido(BaseModel):
+        vendedor : str
+        mes: int
+        valor: float
+        qnt_vendas : int
 
 app = FastAPI()
-
-vendedor1 = 0
-comissao = {
-    for (Pedidos[vendedor:1]){
-        vendedor1 = sum(Pedidos["valor"])
-    }
-
-}
-
-calcula_comissao = {
-
-}
-Pedidos = {
-     {"vendedor": 1, "data": "2022-03-01", "valor": 500.34},
-     {"vendedor": 1, "data": "2022-03-01", "valor": 1000.22},
-     { "vendedor": 1, "data": "2022-03-01", "valor": 100.35},
-     {"vendedor": 1, "data": "2022-03-01", "valor": 100.35},
-     {"vendedor": 1, "data": "2022-03-01", "valor": 100.35},
-     {"vendedor": 1, "data": "2022-03-01", "valor": 22.34},
-     {"vendedor": 1, "data": "2022-04-01", "valor": 5000.34 },
-     {"vendedor": 2, "data": "2022-03-01", "valor": 2000.34 },
-     {"vendedor": 2, "data": "2022-04-01", "valor": 3000.34 },
-     {"vendedor": 3, "data": "2022-03-01", "valor": 500.34},
-     {"vendedor": 3, "data": "2022-03-01", "valor": 1000.22},
-     {"vendedor": 4, "data": "2022-03-01", "valor": 100.35 },
-     {"vendedor": 4, "data": "2022-03-01", "valor": 22.34 },
-     {"vendedor": 3, "data": "2022-04-01", "valor": 5000.34 },
-     {"vendedor": 2, "data": "2022-03-01", "valor": 2000.34 },
-     {"vendedor": 2, "data": "2022-04-01", "valor": 3000.34 },
-
-
-}
-pedidos_df = pd.DataFrame(Pedidos)
 
 
 @app.get("/")
 async def home():
     return {"Calcula comissão de vendedores"}
 
-@app.get("/pedidos/{id_pedidos}")
-async def pegar_pedidos(id_pedidos: int):
-        if id_pedidos in Pedidos:
-           return Pedidos[id_pedidos]
-        else:
-            return{ "Erro":"ID pedidos inexistente"}
+@app.post("/calcular", status_code=201)
+async def insere_pedidos(pedido: Pedido): #Comissao adicional por atingir meta
+    mes=pedido.mes
+    if pedido.qnt_vendas >= 5 and mes == 1 or mes == 5:
+        comissao_mes = pedido.valor * 0.03
+    elif pedido.qnt_vendas >= 3 and mes == 2:
+        comissao_mes = pedido.valor * 0.03
+    elif pedido.qnt_vendas >= 2 and mes == 3 or mes == 4 or mes == 8 or mes == 12:
+        comissao_mes = pedido.valor * 0.03
+    elif pedido.qnt_vendas >= 4 and mes == 9 or mes == 10:
+        comissao_mes = pedido.valor * 0.03
+    elif pedido.qnt_vendas >= 60 and mes == 6:
+        comissao_mes = pedido.valor * 0.03
+    elif pedido.qnt_vendas >=7 and mes == 11:
+        comissao_mes = pedido.valor * 0.03
+    else:
+            comissao_mes = 0
 
-@app.post("/comissoes", status_code=201)
-async def insere_pedidos(pedidos: str):
-         return {"pedidos"}
+
+
+    if pedido.valor <= 300.00:
+        comissao_por_quantidade = pedido.valor * 0.01
+        calculo_comissao = comissao_por_quantidade + comissao_mes
+        return ("O valor da comissão será: R$", calculo_comissao)
+
+    if pedido.valor > 300.00 and pedido.valor <= 1000.00:
+        comissao_por_quantidade = pedido.valor * 0.03
+        calculo_comissao = comissao_por_quantidade + comissao_mes
+        return ("O valor da comissão será: R$", calculo_comissao)
+
+    if pedido.valor > 1000.00:
+        comissao_por_quantidade = pedido.valor * 0.05
+        calculo_comissao = comissao_por_quantidade + comissao_mes
+        return ("O valor da comissão será: R$", calculo_comissao)
